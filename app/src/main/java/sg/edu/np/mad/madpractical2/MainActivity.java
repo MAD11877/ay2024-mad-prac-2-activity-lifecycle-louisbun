5d160884a9f0eee87f6508f1fe019192b4b056e9
louis <s10257445@connect.np.edu.sg>
2024-04-24 20:29:52 +0800

    Practical 2, 4.5 done

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -1,12 +1,14 @@
 package sg.edu.np.mad.madpractical2;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +22,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize a new user object
+        User user = new User("John Doe", "MAD Developer", 1, false);
+
+        // Get the TextViews and Button from the layout
+        TextView tvName = findViewById(R.id.textView3);
+        TextView tvDescription = findViewById(R.id.textView2);
+        Button btnFollow = findViewById(R.id.button);
+
+        // Set the TextViews with the User's name, description and default button message
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText("Follow");
     }
+
 }
